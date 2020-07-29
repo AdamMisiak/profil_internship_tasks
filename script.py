aec7f0c6c6bcf5a5ba1e2ce226d4e94ef2f31f43
AdamMisiak <adammisiak3@gmail.com>
2020-07-29 20:19:18 +0200

    creating full user's info added

--- script.py
+++ script.py
@@ -15,13 +15,25 @@ def script():
 	data = data['results']
 	for person in data:
 
-
-		print((person['location']['city']))
-		# person = Person(gender=person['gender'], title=person['name']['title'], first=person['name']['first'],
-		# 	last=person['name']['last'], street_number=person['location']['street']['number'],
-	# 		street_name=person['location']['street']['name'], city=person['location']['city'],
-# 			state=person['location']['city']	)
-		# person.save()
+		print((person['nat']))
+		person = Person(gender=person['gender'], title=person['name']['title'], first=person['name']['first'],
+			last=person['name']['last'], street_number=person['location']['street']['number'],
+			street_name=person['location']['street']['name'], city=person['location']['city'],
+			state=person['location']['state'], country=person['location']['country'],
+			postcode=person['location']['postcode'], latitude=person['location']['coordinates']['latitude'],
+			longitude=person['location']['coordinates']['latitude'],
+			timezone_offset=person['location']['timezone']['offset'],
+			timezone_description=person['location']['timezone']['description'], email=person['email'],
+			login_uuid=person['login']['uuid'], username=person['login']['username'],
+			password=person['login']['password'], password_salt=person['login']['salt'],
+			password_md5=person['login']['md5'], password_sha1=person['login']['sha1'],
+			password_sha256=person['login']['sha256'], dob=person['dob']['date'],
+			age=person['dob']['age'], registered_date=person['registered']['date'],
+			registered_age=person['registered']['age'], phone=person['phone'], cell=person['cell'],
+			id_name=person['id']['name'], id_value=person['id']['value'],
+			picture_large=person['picture']['large'], picture_medium=person['picture']['medium'],
+			thumbnail=person['picture']['thumbnail'], nat=person['nat'])
+		person.save()
 
 
 script()
