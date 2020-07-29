8d580823141b4e06713f8508daec899cea98f799
AdamMisiak <adammisiak3@gmail.com>
2020-07-29 19:13:42 +0200

    person's model changed and basic info taken from json

--- script.py
+++ script.py
@@ -5,8 +5,8 @@ import os
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'profil_intern.settings'
 django.setup()
-
 Person = apps.get_model('queries', 'Person')
+
 
 def script():
 	with open('queries/persons.json') as file:
@@ -15,9 +15,13 @@ def script():
 	data = data['results']
 	for person in data:
 
-		print(person['gender'])
-		person = Person(gender=person['gender'])
-		person.save()
+
+		print((person['location']['city']))
+		# person = Person(gender=person['gender'], title=person['name']['title'], first=person['name']['first'],
+		# 	last=person['name']['last'], street_number=person['location']['street']['number'],
+	# 		street_name=person['location']['street']['name'], city=person['location']['city'],
+# 			state=person['location']['city']	)
+		# person.save()
 
 
 script()
